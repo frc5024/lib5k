f7c3345b63cee96a18c39b28eba9f54bf99e3d7e
Evan Pratten <ewpratten@gmail.com>
2019-09-14 22:48:45 -0400

    Cleaned up some systems, added LED driver code

---
+++ components/BlinkinDriver.java
@@ -0,0 +1,68 @@
+package frc.lib5k.components;
+
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+
+/**
+ * A wrapper for the REV Blinkin LED driver <br>
+ * http://www.revrobotics.com/rev-11-1105/
+ */
+public class BlinkinDriver extends SendableBase {
+
+    enum LEDSetting {
+        RED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        // kRED(0.61),
+        BLUE(0.61);
+
+        private double m_pwmVal;
+
+        LEDSetting(double pwm_val) {
+            m_pwmVal = pwm_val;
+        }
+
+        public double get() {
+            return m_pwmVal;
+        }
+
+    }
+
+    Spark m_controller;
+
+    /**
+     * A wrapper for the REV Blinkin LED driver
+     * 
+     * @param pwm_port The PWM channel for the deivce
+     */
+    public BlinkinDriver(int pwm_port) {
+        m_controller = new Spark(pwm_port);
+    }
+
+    /**
+     * Set the LED driver to a specific setting
+     * 
+     * @param setting LED pattern
+     */
+    public void set(LEDSetting setting) {
+        m_controller.set(setting.get());
+    }
+
+    /**
+     * This method handles interaction with some internal WPIlib services
+     */
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        // TODO Auto-generated method stub
+
+    }
+
+}

be930acaf4e333e0607e5af5dd1b4fb19d2e3ffe
Evan Pratten <ewpratten@gmail.com>
2020-07-03 14:52:00 -0400

    add encoder simulation

---
+++ hardware/common/src/main/java/io/github/frc5024/lib5k/hardware/common/sensors/interfaces/EncoderSimulation.java
@@ -0,0 +1,20 @@
+package io.github.frc5024.lib5k.hardware.common.sensors.interfaces;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import io.github.frc5024.lib5k.utils.interfaces.PeriodicComponent;
+
+public interface EncoderSimulation extends PeriodicComponent {
+
+    /**
+     * Init simulation for this encoder
+     * 
+     * @param controller    The motor controller that controls the encoder
+     * @param gearbox_ratio The gear ratio between the motor and encoder
+     * @param max_rpm       The max RPM of the motor
+     * @param ramp_time     The approx amount of time it takes for the motor to go
+     *                      from 0-100% under load
+     */
+    public void initSimulationDevice(SpeedController controller, double gearbox_ratio, double max_rpm,
+            double ramp_time);
+
+}

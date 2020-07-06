8b132ee09118f56694762e5e91ac5e4506253a3c
Evan Pratten <ewpratten@gmail.com>
2020-07-05 22:49:14 -0400

    Update talon to use new simulation

--- hardware/ctre/src/main/java/io/github/frc5024/lib5k/hardware/ctre/sensors/TalonEncoder.java
+++ hardware/ctre/src/main/java/io/github/frc5024/lib5k/hardware/ctre/sensors/TalonEncoder.java
@@ -1,36 +1,18 @@
 package io.github.frc5024.lib5k.hardware.ctre.sensors;
 
-import io.github.frc5024.lib5k.hardware.common.sensors.interfaces.CommonEncoder;
+import io.github.frc5024.lib5k.hardware.common.sensors.EncoderSimUtil;
 import io.github.frc5024.lib5k.hardware.common.sensors.interfaces.EncoderSimulation;
-import io.github.frc5024.lib5k.control_loops.SlewLimiter;
-import io.github.frc5024.lib5k.hardware.ni.roborio.fpga.FPGAClock;
 
 import com.ctre.phoenix.motorcontrol.can.BaseTalon;
 
-import edu.wpi.first.hal.SimDouble;
-import edu.wpi.first.hal.SimBoolean;
-import edu.wpi.first.hal.SimDevice;
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class TalonEncoder implements CommonEncoder, EncoderSimulation {
+public class TalonEncoder implements EncoderSimulation {
 
     private BaseTalon talon;
     private boolean phase = false;
     private int cpr;
-
-    /* Simulation vars */
-    private SpeedController controller;
-    private double last_time;
-    private double gearbox_ratio, max_rpm;
-
-    /* Simulation */
-    private SimDevice m_simDevice;
-    private SimDouble m_simTicks;
-    private SimDouble m_simRotations;
-    private SimDouble m_simVelocity;
-    private SimBoolean m_simInverted;
-    private static int s_instanceCount = 0;
-    private SlewLimiter m_simSlew;
+    private EncoderSimUtil sim;
 
     public TalonEncoder(BaseTalon talon, int cpr) {
         this.talon = talon;
@@ -40,8 +22,8 @@ public class TalonEncoder implements CommonEncoder, EncoderSimulation {
     @Override
     public void setPhaseInverted(boolean inverted) {
         // Handle simulation vs reality
-        if (m_simDevice != null) {
-            m_simInverted.set(inverted);
+        if (sim.simReady()) {
+            sim.setInverted(inverted);
         } else {
             this.phase = inverted;
             talon.setSensorPhase(inverted);
@@ -51,10 +33,10 @@ public class TalonEncoder implements CommonEncoder, EncoderSimulation {
 
     @Override
     public boolean getInverted() {
-        
+
         // Handle simulation
-        if (m_simDevice != null) {
-            return m_simInverted.get();
+        if (sim.simReady()) {
+            return sim.getInverted();
         }
 
         return this.phase;
@@ -63,8 +45,8 @@ public class TalonEncoder implements CommonEncoder, EncoderSimulation {
     @Override
     public double getPosition() {
         // Handle simulation
-        if (m_simDevice != null) {
-            return m_simTicks.get();
+        if (sim.simReady()) {
+            return sim.getRotations();
         }
         return talon.getSelectedSensorPosition() / this.cpr;
     }
@@ -72,8 +54,8 @@ public class TalonEncoder implements CommonEncoder, EncoderSimulation {
     @Override
     public double getVelocity() {
         // Handle simulation
-        if (m_simDevice != null) {
-            return m_simVelocity.get();
+        if (sim.simReady()) {
+            return sim.getVelocity();
         }
         return talon.getSelectedSensorVelocity() / 1000 / this.cpr;
     }
@@ -81,48 +63,14 @@ public class TalonEncoder implements CommonEncoder, EncoderSimulation {
     @Override
     public void initSimulationDevice(SpeedController controller, double gearbox_ratio, double max_rpm,
             double ramp_time) {
-        // Set locals
-        this.controller = controller;
-        this.gearbox_ratio = gearbox_ratio;
-        this.max_rpm = max_rpm;
-        this.m_simSlew = new SlewLimiter(ramp_time);
 
-        // Init sim device
-        m_simDevice = SimDevice.create("TalonEncoder", s_instanceCount + 1);
-
-        if (m_simDevice != null) {
-            m_simTicks = m_simDevice.createDouble("Ticks", false, 0.0);
-            m_simRotations = m_simDevice.createDouble("Rotations", true, 0.0);
-            m_simVelocity = m_simDevice.createDouble("RPM", true, 0.0);
-            m_simInverted = m_simDevice.createBoolean("Inverted", true, false);
-        }
-
-        // Move to next instance
-        s_instanceCount++;
+        sim = new EncoderSimUtil("TalonSRX Encoder", talon.getDeviceID(), cpr, controller, gearbox_ratio, max_rpm,
+                ramp_time);
     }
 
     @Override
     public void update() {
-        // Handle simulation updates
-        if (m_simDevice != null) {
-            // If this is the first loop, simply re-set the timer, and skip
-            if (last_time == 0) {
-                last_time = FPGAClock.getFPGASeconds();
-                return;
-            }
-
-            // Determine dt
-            double current_time = FPGAClock.getFPGASeconds();
-            double dt = current_time - last_time;
-            last_time = current_time;
-
-            // Calc encoder position
-            double rpm = (m_simSlew.feed(controller.get()) * max_rpm) / gearbox_ratio;
-            double revs = (rpm / 60.0) * dt; // RPM -> RPS -> Multiply by seconds to find rotations since last update
-            m_simTicks.set((int) (m_simTicks.get() + (revs * cpr)));
-            m_simRotations.set((m_simRotations.get() + revs));
-            m_simVelocity.set(rpm);
-        }
+        sim.update();
     }
 
 }

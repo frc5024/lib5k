bea87fe1cc8aefdcef27fa33a6b68c105569b372
Evan Pratten <ewpratten@gmail.com>
2020-07-11 10:02:54 -0400

    Add log parsing

--- scripts/logreader.py
+++ scripts/logreader.py
@@ -5,6 +5,80 @@
 # is produced my a robot program.
 
 import argparse
+import os
+from typing import Iterator
+import re
+
+try:
+    import paramiko
+except:
+    print("Paramiko not installed. Please install it with the following command:")
+    print("python3 -m pip install paramiko")
+    exit(1)
+    
+# In case NI chenges the filename:
+LOGFILE_NAME = "FRC_UserProgram.log"
+
+def simulationLogsExist() -> bool:
+    """Checks to see if a local simulation log output file can be found
+
+    Returns:
+        bool: Does the file exist?
+    """
+    return os.path.exists(f"./{LOGFILE_NAME}")
+
+def localFollow(file) -> Iterator[str]:
+    """ Yield each line from a file as they are written. """
+    line = ''
+    while True:
+        tmp = file.readline()
+        if tmp is not None:
+            line += tmp
+            if line.endswith("\n"):
+                yield line
+                line = ''
+        else:
+            time.sleep(0.1)
+
+def filterLogLine(line: str, levels: list, classes: list, methods: list) -> str:
+    """Filter a log line based on settings. This is probably the only logic that might change year-to-year
+
+    Args:
+        line (str): Raw line from logfile
+        levels (list): Log levels to let through
+        classes (list): Classes to let through
+        methods (list): Methods to let through
+
+    Returns:
+        str: Filtered line
+    """
+
+    # Pass through regex
+    data = re.findall(r"(.*) at (.*)s: (.*)::(.*)\(\) -> (.*)", line, re.M)[0]
+    level, timestamp, _class, method, message = data[0], data[1], data[2], data[3], data[4]
+
+    # This will be modified, then used to determine if we log this line
+    doLog = True
+
+    # Check log level
+    if not level in levels:
+        doLog = False
+
+    # Check class
+    if classes != [''] and _class.upper() not in classes:
+        doLog = False
+    
+
+    # Check method
+    if methods != [''] and method.upper() not in methods:
+        doLog = False
+    
+
+    if doLog:
+        return line.strip() + "\n"
+    else:
+        return ""
+
 
 if __name__ == "__main__":
 
@@ -14,12 +88,12 @@ if __name__ == "__main__":
     
     # Add all arguments
     ap.add_argument("team", help="FRC Team number of the robot or simulation")
+    ap.add_argument("-f", "--follow", help="Setting this flag will follow the logs instead of just dumping a snapshot", required=False, action="store_true")
     ap.add_argument("-i", "--ip", help="Robot or simulation IP address", required=False)
     ap.add_argument("-l", "--local-simulation", help="Use this flag if connecting to a simulation running on the same computer", required=False, action="store_true")
-    ap.add_argument("-d", "--no-driverstation", help="This flag must be set if connecting to a robot without a DriverStation connected", required=False, action="store_true")
     ap.add_argument("-t", "--level-types", help="Comma-seperated list of allowed log levels. Options: DEBUG, INFO, WARNING", required=False)
-    ap.add_argument("-c", "--class", help="Comma-seperated list of class names. This can be used to only view logs from a specific list of classes", required=False)
-    ap.add_argument("-m", "--mathod", help="Comma-seperated list of method names. This can be used to only view logs from a specific list of methods", required=False)
+    ap.add_argument("-c", "--classes", help="Comma-seperated list of class names. This can be used to only view logs from a specific list of classes", required=False)
+    ap.add_argument("-m", "--methods", help="Comma-seperated list of method names. This can be used to only view logs from a specific list of methods", required=False)
     ap.add_argument("-v", "--verbose", help="For logreader debugging", required=False, action="store_true")
 
     # Parse the args
@@ -42,9 +116,64 @@ if __name__ == "__main__":
     if args.ip:
         ip = args.ip
     
+    # Determine list of levels allowed
+    if args.level_types:
+        levels = [x.strip().upper() for x in args.level_types.split(",")]
+    else:
+        levels = ["DEBUG","INFO","WARNING"]
+
+    # Determine list of classes allowed
+    args.classes = args.classes if args.classes else ""
+    classes = [x.strip().upper() for x in args.classes.split(",")]
+
+    # Determine list of methods allowed
+    args.methods = args.methods if args.methods else ""
+    methods = [x.strip().upper() for x in args.methods.split(",")]
+
 
     # If debugging enabled, list parsed args
     if args.verbose:
+        print("-- Argparse Debug --")
         print(f"Team: {args.team}")
-        print(f"Ip address: {ip}")
+        print(f"IP address: {ip}")
+        print(f"Follow Logs: {args.follow}")
+        print(f"Log levels: {levels}")
+        print(f"Classes: {classes}")
+        print(f"Methods: {methods}")
 
+    # Handle simulation vs real
+    if args.local_simulation:
+
+        # Check for local sim file
+        if not simulationLogsExist():
+            print("Can not find local simulation logfile. Please check that:")
+            print(" - A simulation is running")
+            print(" - The simulation is using the Lib5K RobotLogger")
+            print(" - This script is running from the root of the simulation source directory")
+            print(f" - A '{LOGFILE_NAME}' file exists")
+            exit(1)
+
+        # Load file
+        logfile = open(LOGFILE_NAME, "r")
+
+        # Handle follow vs non-follow
+        if args.follow:
+            print("Following logfile. Press CTRL+C to stop.")
+
+            try:
+                # Print all lines as they are written
+                for line in localFollow(logfile):
+                    print(filterLogLine(line, levels, classes, methods), end="")
+
+            except KeyboardInterrupt as e:
+                exit(0)
+
+        else:
+
+            # Just dump the file
+            for line in logfile.read().split("\n"):
+                print(filterLogLine(line, levels, classes, methods), end="")
+            exit(0)
+
+
+        

3f77cdc5b59cb644647986b9bc0b75cb979f7d18
Evan Pratten <ewpratten@gmail.com>
2020-07-14 22:59:07 -0400

    CSVLog TalonSRX

--- hardware/ctre/src/main/java/io/github/frc5024/lib5k/hardware/ctre/motors/ExtendedTalonSRX.java
+++ hardware/ctre/src/main/java/io/github/frc5024/lib5k/hardware/ctre/motors/ExtendedTalonSRX.java
@@ -6,34 +6,50 @@ import edu.wpi.first.wpilibj.RobotBase;
 import io.github.frc5024.lib5k.hardware.common.sensors.interfaces.CommonEncoder;
 import io.github.frc5024.lib5k.hardware.ctre.sensors.TalonEncoder;
 import io.github.frc5024.lib5k.hardware.ni.roborio.fpga.RR_HAL;
+import io.github.frc5024.lib5k.csvlogging.LoggingObject;
+import io.github.frc5024.lib5k.csvlogging.StatusLogger;
 
 /**
- * The ExtendedTalonSRX contains two extra features from WPI_TalonSRX:
- *  - Ablilty to get the attached sensor as a CommonEncoder object
- *  - Small fixes for 2020 simulation voltage bugs in HALSIM
+ * The ExtendedTalonSRX contains two extra features from WPI_TalonSRX: - Ablilty
+ * to get the attached sensor as a CommonEncoder object - Small fixes for 2020
+ * simulation voltage bugs in HALSIM
  */
 public class ExtendedTalonSRX extends WPI_TalonSRX {
     public CTREConfig config;
 
-    public ExtendedTalonSRX(int id){
+    // CSV Logging
+    private LoggingObject csvLog;
+
+    public ExtendedTalonSRX(int id) {
         this(id, new CTREConfig());
     }
-
 
     public ExtendedTalonSRX(int id, CTREConfig config) {
         super(id);
         this.config = config;
-        
+
+        // Set up csv logging
+        csvLog = StatusLogger.getInstance().createLoggingObject(String.format("ExtendedTalonSRX_%d", id), "voltage",
+                "inverted");
+
     }
 
     /**
      * Get the attached encoder
+     * 
      * @return Encoder
      */
     public CommonEncoder getCommonEncoder(int cpr) {
         return new TalonEncoder(this, cpr);
     }
-    
+
+    @Override
+    public void setInverted(boolean inverted) {
+        // Set log value
+        csvLog.setValue("inverted", inverted);
+        super.setInverted(inverted);
+    }
+
     @Override
     public double getMotorOutputVoltage() {
         if (RobotBase.isSimulation()) {
@@ -50,17 +66,18 @@ public class ExtendedTalonSRX extends WPI_TalonSRX {
         } else {
             super.setVoltage(outputVolts);
         }
+
+        // Set log value
+        csvLog.setValue("voltage", outputVolts);
     }
 
-    public ExtendedTalonSRX makeSlave(int id){
+    public ExtendedTalonSRX makeSlave(int id) {
         ExtendedTalonSRX slave = CTREMotorFactory.createTalonSRX(id, this.config);
 
         slave.follow(this);
 
         return slave;
 
-
     }
-
 
 }

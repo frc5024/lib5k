fecbc511e93a25a3681214459e38b7472d9f1cb7
Evan Pratten <ewpratten@gmail.com>
2020-07-18 15:30:45 -0400

    reset pose at path start

--- examples/src/main/java/io/github/frc5024/lib5k/examples/autonomous_path_following/sequences/ForwardOneMeter.java
+++ examples/src/main/java/io/github/frc5024/lib5k/examples/autonomous_path_following/sequences/ForwardOneMeter.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import io.github.frc5024.lib5k.autonomous.AutonomousSequence;
 import io.github.frc5024.lib5k.examples.autonomous_path_following.subsystems.DriveTrain;
 import io.github.frc5024.purepursuit.pathgen.Path;
@@ -17,8 +19,18 @@ public class ForwardOneMeter implements AutonomousSequence {
 
     @Override
     public CommandBase getCommand() {
-        return DriveTrain.getInstance().createPathingCommand(new Path(getStartingPose().getTranslation(),
-                getStartingPose().getTranslation().plus(new Translation2d(1.0, 0.0))), false, 0.2, 0.1);
+        SequentialCommandGroup output = new SequentialCommandGroup();
+    
+        // Pose set command
+        output.addCommands(new InstantCommand(() -> {
+            DriveTrain.getInstance().setPose(getStartingPose());
+        }));
+
+        // Path follow command
+        output.addCommands(DriveTrain.getInstance().createPathingCommand(new Path(getStartingPose().getTranslation(),
+                getStartingPose().getTranslation().plus(new Translation2d(1.0, 0.0))), false, 0.2, 0.1));
+
+        return output;
     }
 
     @Override

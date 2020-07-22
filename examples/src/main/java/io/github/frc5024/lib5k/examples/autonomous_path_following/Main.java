f1c4c61b8527ac494d87d6de96722277da0f07b1
Evan Pratten <ewpratten@gmail.com>
2020-07-22 15:33:15 -0400

    Add a simple joystick drive command

--- examples/src/main/java/io/github/frc5024/lib5k/examples/autonomous_path_following/Main.java
+++ examples/src/main/java/io/github/frc5024/lib5k/examples/autonomous_path_following/Main.java
@@ -2,12 +2,16 @@ package io.github.frc5024.lib5k.examples.autonomous_path_following;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import io.github.frc5024.lib5k.autonomous.RobotProgram;
+import io.github.frc5024.lib5k.examples.autonomous_path_following.commands.DriveCommand;
 import io.github.frc5024.lib5k.examples.autonomous_path_following.sequences.ForwardOneMeter;
 import io.github.frc5024.lib5k.examples.autonomous_path_following.subsystems.DriveTrain;
 import io.github.frc5024.lib5k.logging.RobotLogger.Level;
 import io.github.frc5024.lib5k.logging.USBLogger;
 
 public class Main extends RobotProgram {
+
+    // Commands
+    private DriveCommand driveCommand;
 
     public static void main(String[] args) {
         RobotBase.startRobot(Main::new);
@@ -25,6 +29,10 @@ public class Main extends RobotProgram {
 
         // Set up subsystems
         DriveTrain.getInstance().register();
+
+        // Set up commands
+        driveCommand = new DriveCommand();
+        DriveTrain.getInstance().setDefaultCommand(driveCommand);
     }
 
     @Override

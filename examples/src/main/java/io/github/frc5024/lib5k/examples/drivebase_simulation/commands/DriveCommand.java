5e2358cc4e2f55e9ebde4c18e5bc9040d1b8e27c
Evan Pratten <ewpratten@gmail.com>
2020-08-09 21:49:57 -0400

    Add an example for simulated drivetrain

---
+++ examples/src/main/java/io/github/frc5024/lib5k/examples/drivebase_simulation/commands/DriveCommand.java
@@ -0,0 +1,28 @@
+package io.github.frc5024.lib5k.examples.drivebase_simulation.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import io.github.frc5024.lib5k.examples.drivebase_simulation.OI;
+import io.github.frc5024.lib5k.examples.drivebase_simulation.subsystems.DriveTrain;
+import io.github.frc5024.lib5k.utils.InputUtils.ScalingMode;
+
+public class DriveCommand extends CommandBase {
+
+    public DriveCommand() {
+        addRequirements(DriveTrain.getInstance());
+    }
+
+    @Override
+    public void initialize() {
+        DriveTrain.getInstance().stop();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        DriveTrain.getInstance().stop();
+    }
+
+    @Override
+    public void execute() {
+        DriveTrain.getInstance().drive(OI.getThrottle(), OI.getTurn(), ScalingMode.CUBIC);
+    }
+}

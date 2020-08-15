1dcf1cd2d584d3101e4133cf0aa9df2c287d180c
Evan Pratten <ewpratten@gmail.com>
2020-08-15 11:43:55 -0400

    Revert "Revert "style cleanup""
    This reverts commit 8fbd1472334ac107cf42a3b1acba427d36e9a29d.

--- utils/src/main/java/io/github/frc5024/lib5k/utils/ColorUtils.java
+++ utils/src/main/java/io/github/frc5024/lib5k/utils/ColorUtils.java
@@ -3,13 +3,20 @@ package io.github.frc5024.lib5k.utils;
 import edu.wpi.first.wpilibj.util.Color;
 import io.github.frc5024.lib5k.utils.annotations.FieldTested;
 
-@FieldTested(year=2020)
+@FieldTested(year = 2020)
 public class ColorUtils {
 
-
-    public static boolean epsilonEquals(Color a, Color b, double eps){
-        return (RobotMath.epsilonEquals(a.red, b.red, eps) && RobotMath.epsilonEquals(a.green, b.green, eps) && RobotMath.epsilonEquals(a.blue, b.blue, eps));
+    /**
+     * Check if two colors are equal to eachother within epsilon
+     * 
+     * @param a   Color A
+     * @param b   Color B
+     * @param eps Acceptable range
+     * @return Is equal
+     */
+    public static boolean epsilonEquals(Color a, Color b, double eps) {
+        return (RobotMath.epsilonEquals(a.red, b.red, eps) && RobotMath.epsilonEquals(a.green, b.green, eps)
+                && RobotMath.epsilonEquals(a.blue, b.blue, eps));
     }
-
 
 }
--- waterfall/src/main/java/io/github/frc5024/waterfall/package-info.java
+++
@@ -1,5 +0,0 @@
-
-/**
- * Contains tools for working with large amounts of data
- */
-package io.github.frc5024.waterfall;

b7f45903d584244f7fed2031fb6c8631378cb826
Evan Pratten <ewpratten@gmail.com>
2020-08-14 23:25:18 -0400

    Revert "Revert "update state docs""
    This reverts commit e6d2536df914b083c7de1ae4f481d4343dd3abb6.

--- libKontrol/src/main/java/io/github/frc5024/libkontrol/statemachines/StateHandler.java
+++ libKontrol/src/main/java/io/github/frc5024/libkontrol/statemachines/StateHandler.java
@@ -15,6 +15,7 @@ public class StateHandler<T> {
      * Create a StateHandler for a given state
      * 
      * @param key    The key for this state (it's name)
+     * @param parent The state machine that owns this object
      * @param action The action to be performed during this state
      */
     public StateHandler(@Nonnull T key, @Nonnull StateMachine<T> parent, @Nonnull Consumer<StateMetadata<T>> action) {
@@ -25,7 +26,8 @@ public class StateHandler<T> {
 
     /**
      * Call the action function
-     * @param isNew Is this the first run?
+     * 
+     * @param isNew        Is this the first run?
      * @param lastStateKey Key of the last state to run
      */
     public void call(boolean isNew, @Nullable T lastStateKey) {

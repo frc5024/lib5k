0ec597c4f700d61655384b89d5ecafb52522a4bf
Evan Pratten <ewpratten@gmail.com>
2020-10-08 11:31:04 -0400

    Start work on framework

---
+++ lib5k/src/main/java/io/github/frc5024/lib5k/control_loops/ControllerFactory.java
@@ -0,0 +1,5 @@
+package io.github.frc5024.lib5k.control_loops;
+
+public class ControllerFactory {
+    
+}
---
+++ lib5k/src/main/java/io/github/frc5024/lib5k/control_loops/models/gains/PIDControllerGains.java
@@ -0,0 +1,21 @@
+package io.github.frc5024.lib5k.control_loops.models.gains;
+
+import edu.wpi.first.wpilibj.Sendable;
+
+public abstract class PIDControllerGains implements Sendable {
+
+    public PIDControllerGains(double Kp, double Ki, double Kd, double Kf) {
+        
+    }
+
+    public double getKp() {
+
+    }
+    public double getKi() {
+        
+    }
+    public double getKd() {
+        
+    }
+    
+}

de8224199bc674347b42dd92c44080ab8b5035d0
Evan Pratten <ewpratten@gmail.com>
2020-10-11 13:04:43 -0400

    Add rotation control

--- lib5k/src/main/java/io/github/frc5024/lib5k/bases/drivetrain/implementations/TankDriveTrain.java
+++ lib5k/src/main/java/io/github/frc5024/lib5k/bases/drivetrain/implementations/TankDriveTrain.java
@@ -1,8 +1,14 @@
 package io.github.frc5024.lib5k.bases.drivetrain.implementations;
 
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import io.github.frc5024.lib5k.bases.drivetrain.AbstractDriveTrain;
+import io.github.frc5024.lib5k.control_loops.base.Controller;
+import io.github.frc5024.lib5k.hardware.ni.roborio.fpga.RR_HAL;
 import io.github.frc5024.lib5k.logging.RobotLogger.Level;
 import io.github.frc5024.lib5k.math.DifferentialDriveMath;
+import io.github.frc5024.lib5k.math.RotationMath;
+import io.github.frc5024.lib5k.utils.RobotMath;
 import io.github.frc5024.lib5k.utils.types.DifferentialVoltages;
 import io.github.frc5024.libkontrol.statemachines.StateMetadata;
 
@@ -14,8 +20,16 @@ public abstract class TankDriveTrain extends AbstractDriveTrain {
     // Open loop control
     private DifferentialVoltages openLoopGoal = null;
 
-    public TankDriveTrain() {
-        
+    // Control loops
+    private Controller distanceController;
+    private Controller rotationController;
+
+    // Timer for actions
+    private Timer actionTimer = new Timer();
+
+    public TankDriveTrain(Controller distanceController, Controller rotationController) {
+        this.distanceController = distanceController;
+        this.rotationController = rotationController;
     }
 
     @Override
@@ -31,6 +45,53 @@ public abstract class TankDriveTrain extends AbstractDriveTrain {
             logger.log("An Open-Loop goal of NULL was passed to TankDriveTrain. Failed to write to motors!",
                     Level.kWarning);
         }
+    }
+
+    @Override
+    protected void handleAutonomousRotation(StateMetadata<State> meta, Rotation2d goalHeading, Rotation2d epsilon) {
+
+        if (meta.isFirstRun()) {
+            logger.log("Switched to rotation control");
+            logger.log(String.format("Turning to %s with epsilon of %s", goalHeading.toString(), epsilon.toString()));
+
+            // Configure the controller
+            rotationController.setEpsilon(epsilon.getDegrees());
+            rotationController.setReference(0);
+
+            // Reset and start the action timer
+            actionTimer.reset();
+            actionTimer.start();
+        }
+
+        // Get the current heading
+        Rotation2d currentHeading = getPose().getRotation();
+
+        // Determine the error from the goal
+        double error = RotationMath.getAngularErrorDegrees(currentHeading, goalHeading);
+
+        // Calculate the needed output
+        double output = rotationController.calculate(error) * RR_HAL.MAXIMUM_BUS_VOLTAGE;
+
+        // Write voltage outputs
+        handleVoltage(output, -output);
+
+        // Handle reaching the goal heading
+        if (rotationController.atReference()) {
+
+            // Reset the controller
+            rotationController.reset();
+
+            // Stop the motors
+            handleVoltage(0, 0);
+            actionTimer.stop();
+
+            // Switch to open loop control
+            setOpenLoop(new DifferentialVoltages());
+
+            // Log success
+            logger.log(String.format("Reached heading goal after %.2f seconds", actionTimer.get()));
+        }
+
     }
 
     protected abstract void handleVoltage(double leftVolts, double rightVolts);

c491407a2596561c8da307d9394b9cdb083c8a90
Evan Pratten <ewpratten@gmail.com>
2020-10-21 14:42:42 -0400

    Remove simulation-related code

--- examples/src/main/java/io/github/frc5024/lib5k/examples/teleop_drive/subsystems/DriveTrain.java
+++ examples/src/main/java/io/github/frc5024/lib5k/examples/teleop_drive/subsystems/DriveTrain.java
@@ -2,17 +2,11 @@ package io.github.frc5024.lib5k.examples.teleop_drive.subsystems;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import io.github.frc5024.common_drive.gearing.Gear;
 import io.github.frc5024.lib5k.bases.drivetrain.implementations.TankDriveTrain;
-import io.github.frc5024.lib5k.control_loops.models.DCBrushedMotor;
 import io.github.frc5024.lib5k.examples.teleop_drive.RobotConfig;
 import io.github.frc5024.lib5k.hardware.common.sensors.interfaces.CommonEncoder;
-import io.github.frc5024.lib5k.hardware.common.sensors.interfaces.EncoderSimulation;
 import io.github.frc5024.lib5k.hardware.ctre.motors.ExtendedTalonSRX;
 import io.github.frc5024.lib5k.hardware.kauai.gyroscopes.NavX;
 
@@ -26,8 +20,8 @@ public class DriveTrain extends TankDriveTrain {
     private ExtendedTalonSRX rightRearMotor;
 
     // Encoders
-    private EncoderSimulation leftEncoder;
-    private EncoderSimulation rightEncoder;
+    private CommonEncoder leftEncoder;
+    private CommonEncoder rightEncoder;
 
     // Gyroscope
     private NavX gyroscope;
@@ -35,12 +29,6 @@ public class DriveTrain extends TankDriveTrain {
     // Motor and encoder inversion multiplier
     private double motorInversionMultiplier = 1.0;
     private double encoderInversionMultiplier = 1.0;
-
-    // Simulation
-    private NetworkTableInstance ntInst;
-    private NetworkTableEntry ntX;
-    private NetworkTableEntry ntY;
-    private NetworkTableEntry ntTheta;
 
     private DriveTrain() {
         super();
@@ -62,32 +50,11 @@ public class DriveTrain extends TankDriveTrain {
         rightFrontMotor.setSensorPhase(false);
 
         // Set up encoders
-        leftEncoder = (EncoderSimulation) leftRearMotor.getCommonEncoder(RobotConfig.DRIVETRAIN_ENCODER_TPR);
-        rightEncoder = (EncoderSimulation) rightRearMotor.getCommonEncoder(RobotConfig.DRIVETRAIN_ENCODER_TPR);
-
-        // Set up motor simulation
-        if (RobotBase.isSimulation()) {
-            leftEncoder.initSimulationDevice(leftFrontMotor, RobotConfig.DRIVETRAIN_GEAR_RATIO,
-                    RobotConfig.DRIVETRAIN_GEARBOX_MOTOR_TYPE.getFreeSpeedRPM(),
-                    RobotConfig.MAXIMUM_TIME_TO_ACCELERATE_SECONDS);
-            rightEncoder.initSimulationDevice(rightFrontMotor, RobotConfig.DRIVETRAIN_GEAR_RATIO,
-                    RobotConfig.DRIVETRAIN_GEARBOX_MOTOR_TYPE.getFreeSpeedRPM(),
-                    RobotConfig.MAXIMUM_TIME_TO_ACCELERATE_SECONDS);
-        }
+        leftEncoder = leftRearMotor.getCommonEncoder(RobotConfig.DRIVETRAIN_ENCODER_TPR);
+        rightEncoder = rightRearMotor.getCommonEncoder(RobotConfig.DRIVETRAIN_ENCODER_TPR);
 
         // Set up gyroscope
         gyroscope = NavX.getInstance();
-
-        // Set up gyro simulation
-        if (RobotBase.isSimulation()) {
-            gyroscope.initDrivebaseSimulation(this);
-        }
-
-        // Set up networktables
-        ntInst = NetworkTableInstance.getDefault();
-        ntX = ntInst.getEntry("drivetrain_x");
-        ntY = ntInst.getEntry("drivetrain_y");
-        ntTheta = ntInst.getEntry("drivetrain_theta");
     }
 
     public static DriveTrain getInstance() {
@@ -157,11 +124,7 @@ public class DriveTrain extends TankDriveTrain {
 
     @Override
     protected void runIteration() {
-        // Publish pose to NT
-        Pose2d pose = getPose();
-        ntX.setDouble(pose.getTranslation().getX());
-        ntY.setDouble(pose.getTranslation().getY());
-        ntTheta.setDouble(pose.getRotation().getDegrees());
+        // Nothing needs to happen here
     }
 
     @Override
